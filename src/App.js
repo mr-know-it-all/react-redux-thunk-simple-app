dd56d0c1a0101ed5e5a66ac4716ee8a00ac6efc9
Paul-Constantin Chitic <paul.constantin.chitic@gmail.com>
2018-06-20 21:26:38 +0300

    _

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
-
 import './App.css';
 import store from './store/';
 import Counter from './components/counter/counter.js';
