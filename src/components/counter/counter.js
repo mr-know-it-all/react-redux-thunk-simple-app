2e703778b59ac1c5ff07ab3afb63e158fb0fbdae
Paul-Constantin Chitic <paul.constantin.chitic@gmail.com>
2018-07-20 08:05:41 +0300

    _

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -2,6 +2,20 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {incrementCounter, decrementCounter} from '../../store/actions.js';
 
+let fetchMock = require('fetch-mock');
+
+fetchMock.get('https://url', (url) => {
+	console.log('GET url', url);
+
+	return {hello: 'World'};
+});
+
+fetchMock.post('https://url', (url, data) => {
+	console.log('POST payload', url, data);
+	return {a: 2, b: 4, c: 5};
+});
+
+
 const Counter = (props) => {
 	return (
 		<div className={'counter-wrapper'}>
@@ -9,9 +23,32 @@ const Counter = (props) => {
 			<p className={'counter-value'}>Counter Value: {props.count}</p>
 			<button className={'counter-incrementer'} onClick={props.incrementCounter}>Increment Counter</button>
 			<button className={'counter-decrementer'} onClick={props.decrementCounter}>Increment Counter</button>
+
+			<button className={'counter-decrementer'} onClick={getSomeData}>Get Some Data</button>
+			<button className={'counter-decrementer'} onClick={postSomeData}>Post Some Data</button>
 		</div>
 	);
 }
+
+function getSomeData() {
+  return fetch('https://url').then(function(response) {
+    return response.json();
+  }).then(function(data) {
+	  console.log('get response: ', data);
+	}, function(e) {
+		console.error(e);
+	});
+};
+
+function postSomeData() {
+  return fetch('https://url', {method: 'POST', body: JSON.stringify({a: 1, b: 2})}).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+	  console.log('post response: ', data);
+	}, function(e) {
+		console.error(e);
+	});
+};
 
 const mapStateToProps = (state) => {
 	return {
